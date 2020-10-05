46b37425c414faf43cb74abd844f44ed164f9a8c
Bill Ang Li <bill.ang.li@hotmail.com>
2020-10-04 21:15:13 -0400

    Change navbar and button hover

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,39 @@
         <li style="float: left">
           <a href="#">
             <h5>Bill Ang Li</h5>
+            <div id="underline"></div>
           </a>
         </li>
         <li>
           <a href="#about">
             <h5>About</h5>
+            <div id="underline"></div>
           </a>
         </li>
         <li>
           <a href="#experience">
             <h5>Experience</h5>
+            <div id="underline"></div>
           </a>
         </li>
         <li>
           <a href="#projects">
             <h5>Projects</h5>
+            <div id="underline"></div>
           </a>
         </li>
         <li>
           <a href="#contact">
             <h5>Contact</h5>
+            <div id="underline"></div>
           </a>
         </li>
         <li>
           <a href="resume.pdf" target="_blank">
             <h5>Resume</h5>
+            <div style="background-color: var(--red)">
+              <div id="underline"></div>
+            </div>
           </a>
         </li>
       </ul>
@@ -47,58 +55,52 @@
         style="padding-top: 20px"
       />
       <h6>Computer Science student at the University of Toronto</h6>
-      <h6>Looking for 2021 New Grad jobs</h6>
+      <h6>Seeking New Grad 2021 opportunities</h6>
     </div>
-    <div id="about">
-      <div class="container">
-        <h1>About Me</h1>
-        <p>
-          I love programming, playing basketball, learning guitar melodies and
-          reading books. I am finishing up my degree of a Specialist in Computer
-          Science and a Minor in Economics.
-        </p>
-      </div>
+    <div id="about" class="container">
+      <h1>About Me</h1>
+      <p>
+        I love programming, playing basketball, learning guitar melodies and
+        reading books. I am finishing up my degree of a Specialist in Computer
+        Science and a Minor in Economics.
+      </p>
     </div>
-    <div id="experience">
-      <div class="container">
-        <h1>Work Experience</h1>
-        <div v-for="job in jobs" :key="job.position">
-          <h2>{{ job.position }}</h2>
-          <div style="clear: both">
-            <h3 class="alignLeft">{{ job.company }}</h3>
-            <h3 class="alignRight">{{ job.date }}</h3>
-          </div>
-          <br />
-          <ul id="job-description">
-            <li v-for="line in job.descriptions" :key="line">{{ line }}</li>
-          </ul>
+    <div id="experience" class="container">
+      <h1>Work Experience</h1>
+      <div v-for="job in jobs" :key="job.position">
+        <h2>{{ job.position }}</h2>
+        <div style="clear: both">
+          <h3 class="alignLeft">{{ job.company }}</h3>
+          <h3 class="alignRight">{{ job.date }}</h3>
         </div>
+        <br />
+        <ul id="job-description">
+          <li v-for="line in job.descriptions" :key="line">{{ line }}</li>
+        </ul>
       </div>
     </div>
-    <div id="projects">
-      <div class="container">
-        <h1>Select Projects</h1>
-        <div
-          v-for="project in projects"
-          :key="project"
-          style="display: flex; margin-bottom: 20px"
-        >
-          <div class="project-img-container">
-            <div class="img-overlay">
-              <a :href="project.url" target="_blank">
-                <img
-                  :src="project.image"
-                  :alt="project.alt"
-                  class="project-img"
-                  style="display: block"
-                />
-              </a>
-            </div>
+    <div id="projects" class="container">
+      <h1>Select Projects</h1>
+      <div
+        v-for="project in projects"
+        :key="project"
+        style="display: flex; margin-bottom: 20px"
+      >
+        <div class="project-img-container">
+          <div class="img-overlay">
+            <a :href="project.url" target="_blank">
+              <img
+                :src="project.image"
+                :alt="project.alt"
+                class="project-img"
+                style="display: block"
+              />
+            </a>
           </div>
-          <div style="margin: 15px">
-            <h2>{{ project.title }}</h2>
-            <h3 style="padding-top: 10px">{{ project.description }}</h3>
-          </div>
+        </div>
+        <div style="margin: 15px">
+          <h2>{{ project.title }}</h2>
+          <h3 style="padding-top: 10px">{{ project.description }}</h3>
         </div>
       </div>
     </div>
@@ -118,7 +120,7 @@
       </div>
     </div>
     <div id="footer">
-      <p style="text-align: center"><strong>Bill Ang Li</strong> - 2020</p>
+      <h4 style="text-align: center"><strong>Bill Ang Li</strong> - 2020</h4>
     </div>
   </div>
 </template>
@@ -201,6 +203,10 @@ export default {
 body {
   margin: 0;
   padding: 0;
+  --blue: #c0f5fa;
+  --red: #fc5f39;
+  --green: #a8de00;
+  --light-gray: #e7ebee;
 }
 
 #app {
@@ -210,14 +216,13 @@ body {
 h1 {
   font-family: "Roboto", sans-serif;
   font-size: 2.5em;
-  padding: 0;
 }
 
 h2 {
   font-family: "Roboto", sans-serif;
   font-size: 1.5em;
   line-height: 0;
-  margin: 30px 0 10px 0;
+  /* margin: 30px 0 10px 0; */
 }
 
 h3 {
@@ -227,11 +232,19 @@ h3 {
   font-style: italic;
 }
 
+h4 {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1em;
+  margin: 0;
+  padding: 1em;
+}
+
 h5 {
   font-family: "Roboto", sans-serif;
   font-size: 20px;
-  line-height: 0;
   text-transform: uppercase;
+  margin-top: 1em;
+  margin-bottom: 0;
 }
 
 h6 {
@@ -263,19 +276,15 @@ p {
   list-style-type: none;
   margin: 0;
   padding: 0;
-  /* position: fixed; */
+  position: fixed;
   top: 0;
   width: 100%;
-  background-color: white;
+  background-color: var(--light-gray);
   text-align: right;
 }
 
 #navbar li {
   display: inline-block;
-}
-
-#navbar li:hover {
-  background-color: #c0f5fa;
 }
 
 #navbar li a {
@@ -286,17 +295,29 @@ p {
   text-decoration: none;
 }
 
+#underline {
+  width: 100%;
+  height: 0.4em;
+  margin-bottom: 0.8em;
+  transition: 0.5s ease;
+}
+
+#navbar a:hover #underline {
+  background-color: var(--green);
+}
+
 #banner {
-  padding-top: 100px;
-  padding-bottom: 40px;
+  padding-top: 8em;
+  padding-bottom: 2em;
   margin: 0;
   text-align: center;
-  background-color: #c0f5fa;
+  background-color: var(--blue);
 }
 
 .container {
   max-width: 800px;
   margin: auto;
+  padding-top: 50px;
 }
 
 .project-img {
@@ -312,10 +333,14 @@ p {
 }
 
 .project-img-container:hover .img-overlay {
-  background-color: #c0f5fa;
+  background-color: var(--green);
 }
 
 #contact {
-  background-color: #c0f5fa;
+  background-color: var(--blue);
+}
+
+#footer {
+  background: var(--light-gray);
 }
 </style>
